3bd9d76e7a9b11be68d3f70679d32725a14b7b8c
Henrique <48599271+henrique2020@users.noreply.github.com>
2024-11-15 12:14:04 -0300

    Criado percurso
    Criado Movimento com rotação
      -  Ao rotacionar o veículo se move e sai do centro
    Importada textura da Kombi
      -  Textura com muito espaço vazio, será feita uma nova textura que ocupe mais espaço
    Iniciada tentativa de importar os vértices de um outro arquivo

---
+++ include/vertices.h
@@ -0,0 +1,9 @@
+#ifndef VERTICES_H_INCLUDED
+#define VERTICES_H_INCLUDED
+
+#include <vector>
+
+std::vector<float> generateVerticesGround();
+std::vector<float> generateVerticesCar();
+
+#endif // VERTICES_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -10,111 +10,90 @@
 #include <stb_image.h>
 #include <Shader.h>
 #include <iostream>
+#include <vertices.h>
 
 // Funções para compilar shaders
-GLuint compileShader(const char* vertexSource, const char* fragmentSource);
 void checkShaderCompileStatus(GLuint shader);
 void checkProgramLinkStatus(GLuint program);
+// Função para carregar textura
+unsigned int loadTexture(const char* path);
 // Processar eventos de teclado
 void processInput(GLFWwindow *window, float *x, float *y, float *z);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-
-void moveCar(int *curva, float *carPosX, float *carPosZ);
-
-// Shaders (cada objeto terá um shader próprio)
-const char* vertexShaderSource = "#version 330 core\n"
-    "layout(location = 0) in vec3 position;\n"
-    "uniform mat4 model;\n"
-    "uniform mat4 view;\n"
-    "uniform mat4 projection;\n"
-    "void main() {\n"
-    "   gl_Position = projection * view * model * vec4(position, 1.0f);\n"
-    "}\n";
-
-const char* fragmentShaderSource1 = "#version 330 core\n"
-    "out vec4 color;\n"
-    "void main() {\n"
-    "   color = vec4(1.0, 0.0, 0.0, 0.0); // Vermelho\n"
-    "}\n";
+// Função para carregar as regras de movimentação do carro
+void moveCar(int *curva, float *carPosX, float *carPosZ, float *rotation);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 // Dados dos objetos (chao e carro)
 float groundVertices[] = {
-    /*
-    -0.5f, 0.0f, -0.5f,  0.0f, 1.0f,
-     0.5f, 0.0f, -0.5f,  1.0f, 1.0f,
-     0.5f, 0.0f,  0.5f,  1.0f, 0.0f,
-     0.5f, 0.0f,  0.5f,  1.0f, 0.0f,
-    -0.5f, 0.0f,  0.5f,  0.0f, 0.0f,
-    -0.5f, 0.0f, -0.5f,  0.0f, 1.0f,
-    */
-    -3.0f, 0.0f,  -3.0f,  0.0f, 1.0f,
-    3.0f, 0.0f, -3.0f,  1.0f, 1.0f,
-    3.0f, 0.0f,  3.0f,  1.0f, 0.0f,
-    3.0f, 0.0f,  3.0f,  1.0f, 0.0f,
-   -3.0f, 0.0f, 3.0f,  0.0f, 0.0f,
-    -3.0f, 0.0f, -3.0f,  0.0f, 1.0f
+    -3.0f,  0.0f, -3.0f,  0.0f, 1.0f,
+     3.0f,  0.0f, -3.0f,  1.0f, 1.0f,
+     3.0f,  0.0f,  3.0f,  1.0f, 0.0f,
+     3.0f,  0.0f,  3.0f,  1.0f, 0.0f,
+    -3.0f,  0.0f,  3.0f,  0.0f, 0.0f,
+    -3.0f,  0.0f, -3.0f,  0.0f, 1.0f
 };
 
 float cubeVertices[] = {
-    //Traseira
-    -0.5f, 0.0f, 0.5f, 0.0f, 0.5f,
-     0.5f, 0.0f, 0.5f, 0.25f, 0.5f,
-     0.5f, 0.5f, 0.5f, 0.25f, 0.75f,
-     0.5f, 0.5f, 0.5f, 0.25f, 0.75f,
-    -0.5f, 0.5f, 0.5f, 0.0f, 0.75f,
-    -0.5f, 0.0f, 0.5f, 0.0f, 0.5f,
+    // Frontal
+    -1.0f,  0.0f,  0.5f,  0.0f,  0.0f,
+    -1.0f,  0.5f,  0.5f,  0.0f,  0.25f,
+    -1.0f,  0.5f,  1.5f,  0.25f, 0.25f,
+    -1.0f,  0.0f,  0.5f,  0.0f,  0.0f,
+    -1.0f,  0.5f,  1.5f,  0.25f, 0.25f,
+    -1.0f,  0.0f,  1.5f,  0.25f, 0.0f,
 
-    //Frontal
-    -0.5f, 0.0f,  1.5f, 0.0f, 0.25f,
-     0.5f, 0.0f,  1.5f, 0.25f, 0.25f,
-     0.5f, 0.5f,  1.5f, 0.25f, 0.5f,
-     0.5f, 0.5f,  1.5f, 0.25f, 0.5f,
-    -0.5f, 0.5f,  1.5f, 0.0f, 0.5f,
-    -0.5f, 0.0f,  1.5f, 0.0f, 0.25f,
+    // Traseira
+     1.0f,  0.0f,  0.5f,  0.25f, 0.0f,
+     1.0f,  0.5f,  0.5f,  0.25f, 0.25f,
+     1.0f,  0.5f,  1.5f,  0.5f,  0.25f,
+     1.0f,  0.0f,  0.5f,  0.25f, 0.0f,
+     1.0f,  0.5f,  1.5f,  0.5f,  0.25f,
+     1.0f,  0.0f,  1.5f,  0.5f,  0.0f,
 
-    //Lateral Esquerda
-    -0.5f, 0.5f,  1.5f, 0.0f, 0.0f,
-    -0.5f, 0.5f, 0.5f, 0.25f, 0.0f,
-    -0.5f, 0.0f, 0.5f, 0.25f, 0.25f,
-    -0.5f, 0.0f, 0.5f, 0.25f, 0.25f,
-    -0.5f, 0.0f,  1.5f, 0.0f, 0.25f,
-    -0.5f, 0.5f,  1.5f, 0.0f, 0.0f,
+    // Lateral Direita
+    -1.0f,  0.0f,  0.5f,  0.5f,  0.0f,
+    -1.0f,  0.5f,  0.5f,  0.5f,  0.25f,
+     1.0f,  0.5f,  0.5f,  1.0f,  0.25f,
+    -1.0f,  0.0f,  0.5f,  0.5f,  0.0f,
+     1.0f,  0.5f,  0.5f,  1.0f,  0.25f,
+     1.0f,  0.0f,  0.5f,  1.0f,  0.0f,
 
-    //Lateral Direita
-     0.5f, 0.5f, 1.5f, 0.25f, 0.0f,
-     0.5f, 0.5f, 0.5f, 0.5f, 0.0f,
-     0.5f, 0.0f, 0.5f, 0.5f, 0.25f,
-     0.5f, 0.0f, 0.5f, 0.5f, 0.25f,
-     0.5f, 0.0f, 1.5f, 0.25f, 0.25f,
-     0.5f, 0.5f, 1.5f, 0.25f, 0.0f,
+    // Lateral Esquerda
+    -1.0f,  0.0f,  1.5f,  0.5f,  0.0f,
+    -1.0f,  0.5f,  1.5f,  0.5f,  0.25f,
+     1.0f,  0.5f,  1.5f,  1.0f,  0.25f,
+    -1.0f,  0.0f,  1.5f,  0.5f,  0.0f,
+     1.0f,  0.5f,  1.5f,  1.0f,  0.25f,
+     1.0f,  0.0f,  1.5f,  1.0f,  0.0f,
 
-    //Inferior
-    -0.5f, 0.0f, 0.5f, 0.25f, 0.5f,
-     0.5f, 0.0f, 0.5f, 0.5f, 0.5f,
-     0.5f, 0.0f, 1.5f, 0.5f, 0.75f,
-     0.5f, 0.0f, 1.5f, 0.5f, 0.75f,
-    -0.5f, 0.0f, 1.5f, 0.25f, 0.75f,
-    -0.5f, 0.0f, 0.5f, 0.25f, 0.5f,
+    // Inferior
+    -1.0f,  0.0f,  0.5f,  0.5f,  0.5f,
+     1.0f,  0.0f,  0.5f,  1.0f,  0.5f,
+     1.0f,  0.0f,  1.5f,  1.0f,  1.0f,
+    -1.0f,  0.0f,  0.5f,  0.5f,  0.5f,
+     1.0f,  0.0f,  1.5f,  1.0f,  1.0f,
+    -1.0f,  0.0f,  1.5f,  1.0f,  0.5f,
 
-    //Superior
-    -0.5f, 0.5f, 0.5f, 0.25f, 0.25f,
-     0.5f, 0.5f, 0.5f, 0.5f, 0.25f,
-     0.5f, 0.5f, 1.5f, 0.5f, 0.5f,
-     0.5f, 0.5f, 1.5f, 0.5f, 0.5f,
-    -0.5f, 0.5f, 1.5f, 0.25f, 0.5f,
-    -0.5f, 0.5f, 0.5f, 0.25f, 0.25f
+    // Superior
+    -1.0f,  0.5f,  1.5f,  0.0f,  0.25f,
+    -1.0f,  0.5f,  0.5f,  0.0f,  0.5f,
+     1.0f,  0.5f,  0.5f,  0.5f,  0.5f,
+    -1.0f,  0.5f,  1.5f,  0.0f,  0.25f,
+     1.0f,  0.5f,  0.5f,  0.5f,  0.5f,
+     1.0f,  0.5f,  1.5f,  0.5f,  0.25f
 };
 
-float factor = 0.001f;
+float factor = 0.0005f;
 float carPosX = 0.0f;
 float carPosZ = 0.0f;
+float rotation = 0.0f;
 
 int curva = 1;
 
-bool visaoAerea = false;
+bool visaoAerea = true;
 
 int main() {
    float x = 0.0f;
@@ -147,9 +126,6 @@ int main() {
         return -1;
     }
 
-    // Compilar os shaders para cada objeto
-    GLuint shaderCar = compileShader(vertexShaderSource, fragmentShaderSource1);
-
     // Configurar buffers de vértices para cada objeto
     GLuint VBOs[2], VAOs[2];
     glGenVertexArrays(2, VAOs);
@@ -178,94 +154,41 @@ int main() {
     Shader groundShader("vertex.glsl", "fragment.glsl");
     Shader carShader("vertex.glsl", "fragment.glsl");
 
-    unsigned int groundTexture, carTexture;
-
-    //Definindo a textura do chão
-    glGenTextures(1, &groundTexture);
-    glBindTexture(GL_TEXTURE_2D, groundTexture);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    int width, height, nrChannels;
-    stbi_set_flip_vertically_on_load(true);
-    unsigned char *data = stbi_load("res/images/pista3.png", &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Falha ao carregar a textura" << std::endl;
-    }
-    stbi_image_free(data);
-
-    groundShader.use();
-    groundShader.setInt("textureToAplicate", 0);
-
-    //Carro
-    glGenTextures(1, &carTexture);
-    glBindTexture(GL_TEXTURE_2D, carTexture);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    stbi_set_flip_vertically_on_load(true);
-    data = stbi_load("res/images/carro3.png", &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Falha ao carregar a textura" << std::endl;
-    }
-    stbi_image_free(data);
-
-    carShader.use();
-    carShader.setInt("textureToAplicate", 0);
+    unsigned int
+        groundTexture = loadTexture("res/images/pista3.png"),
+        carTexture = loadTexture("res/images/kombi.png");
 
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glActiveTexture(GL_TEXTURE0);
 
     // Loop de renderização
     while (!glfwWindowShouldClose(window)) {
-
         // Atender os eventos
         processInput(window, &x, &y, &z);
 
         // Limpar a tela
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // Renderizar cada objeto com seu shader
-        glm::mat4 model         = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+
+        glm::mat4 model         = glm::mat4(1.0f);
         glm::mat4 view          = glm::mat4(1.0f);
         glm::mat4 projection    = glm::mat4(1.0f);
         //model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.0f, 0.5f, 0.0f));
         if(visaoAerea){
-            model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-            view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -6.0f));
-            projection = glm::perspective(glm::radians(90.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+            model       = glm::rotate(model, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+            view        = glm::translate(view, glm::vec3(0.0f, 0.0f, -6.0f));
+            projection  = glm::perspective(glm::radians(90.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         } else {
-            model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.5f, 0.0f));
-            view  = glm::translate(view, glm::vec3(0.5f, -1.75f, -6.35f));
-            projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        }
+            model       = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.5f, 0.0f));
+            view        = glm::translate(view, glm::vec3(0.5f, -1.75f, -6.35f));
+            projection  = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);        }
 
-
-        glActiveTexture(GL_TEXTURE0);
-
+        // Renderizar cada objeto com seu shader
         // Solo
         glBindTexture(GL_TEXTURE_2D, groundTexture);
         groundShader.use();
-
         unsigned int modelLoc = glGetUniformLocation(groundShader.ID, "model");
         unsigned int viewLoc  = glGetUniformLocation(groundShader.ID, "view");
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
@@ -274,19 +197,17 @@ int main() {
         glBindVertexArray(VAOs[0]);
         glDrawArrays(GL_TRIANGLES, 0, 6);
 
-
         // Carro
         // Bind da textura e shader
         glBindTexture(GL_TEXTURE_2D, carTexture);
         carShader.use();
-
-
         // Mover o carro
-        moveCar(&curva, &carPosX, &carPosZ);
+        moveCar(&curva, &carPosX, &carPosZ, &rotation);
         model = glm::translate(model, glm::vec3(carPosX, 0.0f, carPosZ));
+        model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+
         //Aplicar escala de 20% no tamanho do carro
         model = glm::scale(model, glm::vec3(0.2f));
-
         // Configuração das matrizes no shader
         modelLoc = glGetUniformLocation(carShader.ID, "model");
         viewLoc  = glGetUniformLocation(carShader.ID, "view");
@@ -295,8 +216,6 @@ int main() {
         carShader.setMat4("projection", projection);
         glBindVertexArray(VAOs[1]);
         glDrawArrays(GL_TRIANGLES, 0, 36);
-
-
 
         // Trocar os buffers da janela
         glfwSwapBuffers(window);
@@ -308,29 +227,6 @@ int main() {
 }
 
 // Funções auxiliares
-GLuint compileShader(const char* vertexSource, const char* fragmentSource) {
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexSource, NULL);
-    glCompileShader(vertexShader);
-    checkShaderCompileStatus(vertexShader);
-
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
-    glCompileShader(fragmentShader);
-    checkShaderCompileStatus(fragmentShader);
-
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    checkProgramLinkStatus(shaderProgram);
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    return shaderProgram;
-}
-
 void checkShaderCompileStatus(GLuint shader) {
     int success;
     glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
@@ -360,8 +256,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window, float *x, float *y, float *z)
-{
+void processInput(GLFWwindow *window, float *x, float *y, float *z) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
@@ -406,15 +301,16 @@ void processInput(GLFWwindow *window, float *x, float *y, float *z)
     }
 }
 
-void moveCar(int *curvaOriginal, float *carPosXOriginal, float *carPosZOriginal){
+void moveCar(int *curvaOriginal, float *carPosXOriginal, float *carPosZOriginal, float *rotationOriginal) {
     int curva = *curvaOriginal;
-    float carPosX = *carPosXOriginal, carPosZ = *carPosZOriginal;
+    float carPosX = *carPosXOriginal, carPosZ = *carPosZOriginal, rotation = *rotationOriginal;
 
     //std::cout << "X: " << carPosX * factor << " Z: " << carPosZ << std::endl;
     switch (curva) {
         case 1:
             carPosX -= 1.0f * factor;
             if (carPosX <= -2.5f) {
+                rotation -= 90.0f;
                 curva = 2;
             }
             break;
@@ -422,6 +318,7 @@ void moveCar(int *curvaOriginal, float *carPosXOriginal, float *carPosZOriginal)
         case 2:
             carPosZ -= 1.0f * factor;
             if (carPosZ <= -2.405f) {
+                rotation -= 90.0f;
                 curva = 3;
             }
             break;
@@ -429,6 +326,7 @@ void moveCar(int *curvaOriginal, float *carPosXOriginal, float *carPosZOriginal)
         case 3:
             carPosX += 1.0f * factor;
             if (carPosX >= -1.5f) {
+                rotation -= 90.0f;
                 curva = 4;
             }
             break;
@@ -436,6 +334,7 @@ void moveCar(int *curvaOriginal, float *carPosXOriginal, float *carPosZOriginal)
         case 4:
             carPosZ += 1.0f * factor;
             if (carPosZ >= -1.925f) {
+                rotation -= 25.0f;
                 curva = 5;
             }
             break;
@@ -444,6 +343,7 @@ void moveCar(int *curvaOriginal, float *carPosXOriginal, float *carPosZOriginal)
             carPosZ += 1.0f * factor;
             carPosX -= 0.5f * factor;
             if (carPosX <= -1.895f && carPosZ >= -1.135f) {
+                rotation += 90.0f;
                 curva = 6;
             }
             break;
@@ -452,6 +352,7 @@ void moveCar(int *curvaOriginal, float *carPosXOriginal, float *carPosZOriginal)
             carPosZ += 0.5f * factor;
             carPosX += 1.0f * factor;
             if (carPosX >= -1.185f && carPosZ >= -0.78f) {
+                rotation += 90.0f;
                 curva = 7;
             }
             break;
@@ -460,6 +361,7 @@ void moveCar(int *curvaOriginal, float *carPosXOriginal, float *carPosZOriginal)
             carPosZ -= 1.0f * factor;
             carPosX += 0.5f * factor;
             if (carPosX >= -0.495f && carPosZ <= -2.16f) {
+                rotation -= 65.0f;
                 curva = 8;
             }
             break;
@@ -467,6 +369,7 @@ void moveCar(int *curvaOriginal, float *carPosXOriginal, float *carPosZOriginal)
         case 8:
             carPosX += 1.0f * factor;
             if (carPosX >= 0.05f) {
+                rotation -= 90.0f;
                 curva = 9;
             }
             break;
@@ -474,6 +377,7 @@ void moveCar(int *curvaOriginal, float *carPosXOriginal, float *carPosZOriginal)
         case 9:
             carPosZ += 1.0f * factor;
             if (carPosZ >= -0.85f) {
+                rotation += 90.0f;
                 curva = 10;
             }
             break;
@@ -481,6 +385,7 @@ void moveCar(int *curvaOriginal, float *carPosXOriginal, float *carPosZOriginal)
         case 10:
             carPosX += 1.0f * factor;
             if (carPosX >= 1.05f) {
+                rotation += 90.0f;
                 curva = 11;
             }
             break;
@@ -488,6 +393,7 @@ void moveCar(int *curvaOriginal, float *carPosXOriginal, float *carPosZOriginal)
         case 11:
             carPosZ -= 1.0f * factor;
             if (carPosZ <= -2.05f) {
+                rotation -= 90.0f;
                 curva = 12;
             }
             break;
@@ -495,6 +401,7 @@ void moveCar(int *curvaOriginal, float *carPosXOriginal, float *carPosZOriginal)
         case 12:
             carPosX += 1.0f * factor;
             if (carPosX >= 1.85f) {
+                rotation -= 90.0f;
                 curva = 13;
             }
             break;
@@ -502,6 +409,7 @@ void moveCar(int *curvaOriginal, float *carPosXOriginal, float *carPosZOriginal)
         case 13:
             carPosZ += 1.0f * factor;
             if (carPosZ >= 1.5f) {
+                rotation -= 90.0f;
                 curva = 14;
             }
             break;
@@ -509,6 +417,7 @@ void moveCar(int *curvaOriginal, float *carPosXOriginal, float *carPosZOriginal)
         case 14:
             carPosX -= 1.0f * factor;
             if (carPosX <= 1.075f) {
+                rotation -= 90.0f;
                 curva = 15;
             }
             break;
@@ -516,11 +425,36 @@ void moveCar(int *curvaOriginal, float *carPosXOriginal, float *carPosZOriginal)
         case 15:
             carPosZ -= 1.0f * factor;
             if (carPosZ <= 0.0f) {
+                rotation = 0;
                 curva = 1;
             }
             break;
     }
 
     *curvaOriginal = curva;
-    *carPosXOriginal = carPosX, *carPosZOriginal = carPosZ;
+    *carPosXOriginal = carPosX, *carPosZOriginal = carPosZ, *rotationOriginal = rotation;
 }
+
+unsigned int loadTexture(const char* path) {
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* data = stbi_load(path, &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture: " << path << std::endl;
+    }
+    stbi_image_free(data);
+    return textureID;
+}
+
